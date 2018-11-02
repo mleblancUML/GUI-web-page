685bc7981db5222bcb314a39f1eabc918b71c714
Matt-Laptop <matthewmleblanc@gmail.com>
2018-11-01 22:18:27 -0400

    Adding unfinished Assignment6 to the github version of my webpage

---
+++ Assignment6/js/assignment6.js
@@ -0,0 +1,11 @@
+var fc, flc, fr, flr;
+
+
+
+
+function getUserInfo() {
+    //What we need to do here is to read in
+    //the information that is in the form.
+    //Then store that information in the vars
+    //available.
+}
