bd01ba550187c64dbe503f9fb7eb1bac98d1d6c1
mleblanc <matthewmleblanc@gmail.com>
2018-12-11 18:42:32 -0500

    Added the gameboard

--- Assignment9/js/assignment9.js
+++ Assignment9/js/assignment9.js
@@ -2,9 +2,12 @@
 // By: Matthew LeBlanc
 // email: matthewmleblanc@gmail.com
 $( function() {
-    $("#draggable").draggable({ snap: ".ui-widget-header" });
+    $("#draggable").draggable({ 
+        snap: ".ui-widget-header",
+        containment: "#containmentWrapper", 
+        scroll: false
+    });
     $("#snapTarget").droppable( { drop: function( event, ui) {
-        console.log("Droppable Target");
-    }
-    })
+        console.log("Target Dropped");
+    }});
 });
