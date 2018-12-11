9b25ed63788f51a0cd0b899e21fdc280916c06b9
Matt <mlbmatt18@gmail.com>
2018-12-11 09:46:34 -0500

    Adding updates to Assignment9

--- Assignment9/js/assignment9.js
+++ Assignment9/js/assignment9.js
@@ -1,3 +1,10 @@
 // Javascript for Assignment 9
 // By: Matthew LeBlanc
 // email: matthewmleblanc@gmail.com
+$( function() {
+    $("#draggable").draggable({ snap: ".ui-widget-header" });
+    $("#snapTarget").droppable( { drop: function( event, ui) {
+        console.log("Droppable Target");
+    }
+    })
+});
---
+++ Assignment9/js/randomize_scribble.js
@@ -0,0 +1,7 @@
+// Javascript file to randomize the tiles, and if I have time, the board as well.
+// By: Matthew LeBlanc
+// email: matthewmleblanc@gmail.com
+console.log("Loading generateTile");
+$("#tile").ready(function() {
+    $("#tile").attr("src", "images/Scrabble_Tile_A.jpg");
+});
