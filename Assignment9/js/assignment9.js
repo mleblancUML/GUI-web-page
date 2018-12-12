6abbe2d932c365d5d6ee233b182f28318d7d4191
Matt <mlbmatt18@gmail.com>
2018-12-12 16:41:53 -0500

    Adding randomized tile creation funcitonality

--- Assignment9/js/assignment9.js
+++ Assignment9/js/assignment9.js
@@ -10,4 +10,38 @@ $( function() {
     $("#snapTarget").droppable( { drop: function( event, ui) {
         console.log("Target Dropped");
     }});
+
+    document.getElementById("board").onload = function() {generateBoard()};
 });
+
+// Adding function to generate board
+function generateBoard() {
+    // Create some variables
+    var i, j;
+    var board = '<div id="snaptarget" class="ui-widget-header">';
+    // Loop through and make the board using indicies from boardTemplate
+    for (i = 0; i < 6; i++) {
+        board += '<img src="Empty_Board_Tile.png">';
+        board += '</div>';
+        if (i == 5) {
+            board += '<br>';
+            if (j == 5) {
+                break;
+            }
+            j++;
+        }
+    }
+    var Board = getElementById("board");
+    Board.innerHTML = board;
+}
+
+//'<img src="images/Scrabble_Tile_' + randGenChar() + '">';
+
+function randGenChar () {
+    console.log("Generating Random Char");
+    var character = "";
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+    var i = 0;
+    character += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    return character;
+}
--- Assignment9/js/randomize_scribble.js
+++ Assignment9/js/randomize_scribble.js
@@ -1,7 +1,14 @@
 // Javascript file to randomize the tiles, and if I have time, the board as well.
 // By: Matthew LeBlanc
 // email: matthewmleblanc@gmail.com
-console.log("Loading generateTile");
+console.log("Loading Tiles");
+// Set the img with proper tile
 $("#tile").ready(function() {
-    $("#tile").attr("src", "images/Scrabble_Tile_A.jpg");
+    $("#tile").attr("src", "images/Scrabble_Tile_" + randGenChar() + '.jpg');
+});
+
+console.log("Loading Board");
+// Set the img with board tile
+$("#boardPiece_empty").each(function() {
+    $(this).attr("src", "images/Empty_Board_Tile.png");
 });
