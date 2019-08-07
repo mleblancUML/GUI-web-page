60c920fd226f857f3241a78829de8e438686459b
George <legeorge4@gmail.com>
2019-08-07 16:43:02 -0400

    First Commit

---
+++ src/js/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Please import your React components that you include in your App
+// Provide all the imports from the various React Components that are to be used
+// import React Component from "./pathto/ReactComponent.jsx";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Your React Components go here</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
