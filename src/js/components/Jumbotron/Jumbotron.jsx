0420de254d411afe170276b2592b4d88547ea817
Matthew.LeBlanc <matthew.leblanc@conceptsio.com>
2020-01-08 17:15:32 -0600

    Adding minor changes

---
+++ src/js/components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Jumbotron extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Welcome to the website of Matt LeBlanc</h1>
+                <p>Filler text for when I implement more modular approach to the jumbotron</p>
+            </div>
+        );
+    }
+}
+
+export default Jumbotron;
